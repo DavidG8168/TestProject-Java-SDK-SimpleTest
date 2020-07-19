69535a27a33e24551e8ecb1ae7e412e710e9d199
David Goichman <45967111+DavidG8168@users.noreply.github.com>
2020-07-19 16:18:34 +0300

    unit test

---
+++ src/test/java/BasicTests.java
@@ -0,0 +1,37 @@
+
+import io.testproject.sdk.drivers.web.ChromeDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class BasicTests {
+
+    @Test
+    public void main() throws Exception {
+
+        ChromeDriver driver = new ChromeDriver("xMOUq-CTwc12Ru7Vjls1syE6uZ8B7vxcv1KAs55XaOY1" ,new ChromeOptions(), "CodeDavidJava");
+
+        driver.navigate().to("https://example.testproject.io/web/");
+
+        driver.report().step("Navigated to site successfully", true);
+
+        driver.findElement(By.cssSelector("#name")).sendKeys("John Smith");
+        driver.findElement(By.cssSelector("#password")).sendKeys("12345");
+        driver.findElement(By.cssSelector("#login")).click();
+
+        driver.report().step("Elements located successfully", true);
+
+        boolean passed = driver.findElement(By.cssSelector("#logout")).isDisplayed();
+
+        if (passed) {
+            driver.report().test("Test Passed", true);
+            System.out.println("Test Passed");
+        } else {
+            driver.report().test("Test Failed", false);
+            System.out.println("Test Failed");
+        }
+
+        driver.quit();
+    }
+}
